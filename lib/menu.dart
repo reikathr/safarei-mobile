5f506a59a721b6b7b7d34f96f958af2fe49a5d2a
meteoreinger <reikathr@gmail.com>
2023-11-05 20:08:07 +0700

    finished checklist tugas 7

---
+++ lib/menu.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+    MyHomePage({Key? key}) : super(key: key);
+
+    final List<InventoryItem> items = [
+        InventoryItem("Lihat Item", Icons.checklist, const Color.fromARGB(255, 114, 101, 238)),
+        InventoryItem("Tambah Item", Icons.add, const Color.fromARGB(255, 236, 88, 78)),
+        InventoryItem("Logout", Icons.logout, const Color.fromARGB(255, 239, 224, 85)),
+    ];
+
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Safarei',
+          style: TextStyle(
+            color: Colors.white,
+          )
+        ),
+        backgroundColor: Colors.black,
+      ),
+      body: SingleChildScrollView(
+        // Widget wrapper yang dapat discroll
+        child: Padding(
+          padding: const EdgeInsets.all(10.0), // Set padding dari halaman
+          child: Column(
+            // Widget untuk menampilkan children secara vertikal
+            children: <Widget>[
+              const Padding(
+                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                // Widget Text untuk menampilkan tulisan dengan alignment center dan style yang sesuai
+                child: Text(
+                  'Safarei Park', // Text yang menandakan toko
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              // Grid layout
+              GridView.count(
+                // Container pada card
+                primary: true,
+                padding: const EdgeInsets.all(20),
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                crossAxisCount: 3,
+                shrinkWrap: true,
+                children: items.map((InventoryItem item) {
+                  // Iterasi untuk setiap item
+                  return InventoryCard(item);
+                }).toList(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+    }
+}
+
+
+class InventoryItem {
+  final String name;
+  final IconData icon;
+  final Color color;
+
+  InventoryItem(this.name, this.icon, this.color);
+}
+
+class InventoryCard extends StatelessWidget {
+  final InventoryItem item;
+
+  const InventoryCard(this.item, {super.key}); // Constructor
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: item.color,
+      child: InkWell(
+        // Area responsive terhadap sentuhan
+        onTap: () {
+          // Memunculkan SnackBar ketika diklik
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(SnackBar(
+                content: Text("Kamu telah menekan tombol ${item.name}!")));
+        },
+        child: Container(
+          // Container untuk menyimpan Icon dan Text
+          padding: const EdgeInsets.all(8),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  item.icon,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
+                const Padding(padding: EdgeInsets.all(3)),
+                Text(
+                  item.name,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
